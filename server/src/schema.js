bc067443272201793aeab9adfb65693155475bb0
Chantelsky <laver.chantelsky@gmail.com>
2021-08-22 22:06:00 +1000

    defining mock data, setting up apollo server

--- server/src/schema.js
+++ server/src/schema.js
@@ -8,16 +8,23 @@ const typeDefs = gql`
   "A track is a group of modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "the tracks title"
     title: String!
+    "the tracks author"
     author: Author!
+    "the tracks main illustration to display in track card or track page detail"
     thumbnail: String
+    "the tracks approx length to complete, in minutes"
     length: Int
+    "the number of modules this track contains"
     modulesCount: Int
   }
 
   type Author {
     id: ID!
+    "the authors first and last name"
     name: String!
+    "authors profile picture url"
     photo: String
   }
 `;

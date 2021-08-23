dce5046376360671df6c940309ad20cdb2803a33
Chantelsky <laver.chantelsky@gmail.com>
2021-08-23 15:10:30 +1000

    fixed a bug that was stopping from querying

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+
 const TrackAPI = require('./datasources/track-api');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  dataSourcs: () => {
+  dataSources: () => {
     return {
       trackAPI: new TrackAPI(),
     };
@@ -15,8 +16,8 @@ const server = new ApolloServer({
 
 server.listen().then(() => {
   console.log(`
-    🚀 Server is running!
-    🔊 listening on port 4000
-    📭 Query at https://studio.apollographql.com/dev
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at https://studio.apollographql.com/dev
   `);
 });

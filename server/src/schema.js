62d794ef19b39bd2668dad556a236b7b533d1ee3
Chantelsky <laver.chantelsky@gmail.com>
2021-08-24 21:29:43 +1000

    updated schema, added resolver chains, built track page

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    "fetch a specific track, provided a track's ID"
     track(id: ID!): Track
   }
   "A track is a group of modules that teaches about a specific topic"
